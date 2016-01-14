82db669240614a1df152d7d5bb015c09dcb7e598
rafaelleru <rafaelleru95103@gmail.com>
2016-01-14 23:13:25 +0100

    cambiadas las rutas para que no sean absolutas

--- revient.sh
+++ revient.sh
@@ -2,6 +2,7 @@
 
 # Para usar, el primer agumento es "nombre_de_usuario" <-- los espacios se reflejan con _ (barra baja),
 # y el segundo argumento es el archivo que queremos enviar
+#como tercer argumento se le puede pasar un archivo log.txt ue indica la linea a partir de la cual enviar el archivo
 
 if [[ $# == 2 ]]; then
   (sleep 1; echo "contact_list"; sleep 1;
@@ -23,11 +24,5 @@ elif [[ $# == 3 ]]; then
         echo "$((linea++)) to $1" > log.txt
         sleep 1
       fi
-    done < "$2";) | /home/rafa/tg/bin/telegram-cli -W -v -k /home/rafa/tg/tg-server.pub
+    done < "$2";) | ~/tg/bin/telegram-cli -W -v -k ~/tg/tg-server.pub
 fi
-
-(sleep 1; echo "contact_list"; sleep 1;
-  while IFS='' read -r line || [[ -n "$line" ]]; do
-     echo "msg $1 $line"
-     sleep 1
-  done < "$2";) | ~/tg/bin/telegram-cli -W -v -k ~/tg/tg-server.pub
