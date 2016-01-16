b2095b34552b8c3f3bd53f12808276c318760a2e
koji3 <jose.linares.1993@gmail.com>
2016-01-16 13:11:40 +0100

    Arreglada repetición de código digna de un suspenso en FP

--- revient.sh
+++ revient.sh
@@ -2,27 +2,23 @@
 
 # Para usar, el primer agumento es "nombre_de_usuario" <-- los espacios se reflejan con _ (barra baja),
 # y el segundo argumento es el archivo que queremos enviar
-#como tercer argumento se le puede pasar un archivo log.txt ue indica la linea a partir de la cual enviar el archivo
+# como tercer argumento se le puede pasar un número que indica la linea a partir de la cual enviar el archivo
 
-if [[ $# == 2 ]]; then
-  (sleep 1; echo "contact_list"; sleep 1;
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-       echo "msg $1 $line"
-       echo "$((linea++)) to $1" > log.txt
-       sleep 1
-    done < "$2";) | ~/tg/bin/telegram-cli -W -v -k ~/tg/tg-server.pub
-elif [[ $# == 3 ]]; then
-  linea=$(cat log.txt | cut -d ' ' -f 1)
-  echo $linea
-  i=0
-  (sleep 1; echo "contact_list"; sleep 1;
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-      if [[ $i -lt $linea ]]; then
-          i=$((i+1))
-      elif [[ $i -ge $linea ]]; then
-        echo "msg $1 $line"
-        echo "$((linea++)) to $1" > log.txt
-        sleep 1
-      fi
-    done < "$2";) | ~/tg/bin/telegram-cli -W -v -k ~/tg/tg-server.pub
+if [[ $# == 3 ]]; then
+	i=$3
+else
+	i=1
 fi
+
+linea=0
+
+(sleep 1; echo "contact_list"; sleep 1;
+while IFS='' read -r line || [[ -n "$line" ]]; do
+   ((linea++));
+   if [[ $linea -lt $i ]]; then continue; fi
+   
+   echo "msg $1 $line"
+   echo "$linea to $1" > log.txt
+   
+   sleep 1
+done < "$2";) #| ~/tg/bin/telegram-cli -W -v -k ~/tg/tg-server.pub
